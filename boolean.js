92ef6c9f36a137c45b21c71c18493d26769805a1
AnkurSinghal27 <41937633+AnkurSinghal27@users.noreply.github.com>
2022-03-08 07:47:40 -0800

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,44 @@
+
+var b= false;
+// console.log(a,b);
+
+var und = undefined;
+// console.log(undefined);
+// console.log(und);
+var und;
+// console.log(und)
+var n = null;
+// console.log(n);
+
+/*two type of data types
+1.primitive datatype; undefine,null,number,string,boolean,symbol
+2.reference data typeof;arrays and objects
+Symbol("")
+*/
+var arr = [1,2,3,4,5]
+var arr = [1,2,"bablu",4,5]
+// console.log(arr);
+// operatos in java scripts
+var a = 36;
+var b = 56;
+console.log("the value of a+b is", a+b);
+console.log("the value of a-b is", a-b);
+console.log("the value of a/b is", a/b);
+console.log("the value of a*b is", a*b);
+
+// assinment operatot
+var c = b;
+c+=b;
+c-=b;
+c*=b;
+c/=b;
+console.log(c);
+// comparision operator
+var x= 34;
+var y= 56;
+console.log(x==y);
+console.log(x<=y);
+console.log(x>=y);
+// logical operator
+console.log(true&&false);
+console.log(true||false);
